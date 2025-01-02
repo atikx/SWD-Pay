90e7f6ddf646dc5cdf724fb51709bfd977a34a07
ATIKSH09 <atikshgupta6373@gmail.com>
2025-01-02 14:36:47 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/question", questionsrouter);
 mongoose.connect(url);
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.json("Server is running");
 });
 
 app.listen(parseInt(PORT), () => {
